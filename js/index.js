b9293a68564cc61f490dcd08a7ee9ed33dc2f8c9
David Klein <dk@generalassemb.ly>
2016-03-20 18:30:01 -0400

    functionality done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
 $(function(){
+  // Execute function on submit
   $("form").submit(swapPic)
 
   function swapPic(event){
     event.preventDefault();
-    var city;
+    var entry;
+    // Get entry from form and store as variable entry
+    entry = $("#city-type").val();
 
-    city = $("#city-type").val();
+    // use logic to figure out which city the entry is
+    // set the background of the page to the appropriate picture
 
+    if (entry === "New York" || entry === "New York City" || entry === "NYC") {
+      $("body").css("background","url(images/nyc.jpg)");
+    }
+
+    else if (entry === "San Francisco" || entry === "SF" || entry === "Bay Area") {
+      $("body").css("background","url(images/sf.jpg)");
+    }
+
+    else if (entry === "Los Angeles" || entry === "LA" || entry === "LAX") {
+      $("body").css("background","url(images/la.jpg)");
+
+    }
+    else if (entry === "Austin" || entry === "ATX") {
+      $("body").css("background","url(images/austin.jpg)");
+    }
+
+    else if (entry === "Sydney" || entry === "SYD") {
+      $("body").css("background","url(images/sydney.jpg)");
+    }
+    // in case entry is not one of our covered cities, display citipix_skyline.jpg
+    else {
+      $("body").css("background","url(images/citipix_skyline.jpg)");
+    };
 
   }
 

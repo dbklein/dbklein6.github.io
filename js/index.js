8c8cceda99e35a33fe43a08e6197856746f752e5
David Klein <dk@generalassemb.ly>
2016-04-18 11:44:18 -0400

    1) tried to make the thumbnails a checkbox as part of the form, but info still isn't sending to formspree.io. 2) added js to keep track of selected items

--- js/index.js
+++ js/index.js
@@ -3,5 +3,17 @@ $(function(){
   // toggle class of selected when clicking on one of the items. selected highlights yellow
   $('.thumbnail').click(function(){
     $(this).toggleClass('selected');
+    updateSelectedItemCount();
   });
+
+  function updateSelectedItemCount() {
+    var selectedItems = $('.selected').length
+    if (selectedItems === 1) {
+      $('.numberOfItemsSelected').text(selectedItems + " item selected");
+    }
+    else {
+      $('.numberOfItemsSelected').text(selectedItems + " items selected");
+    };
+
+  };
 });
